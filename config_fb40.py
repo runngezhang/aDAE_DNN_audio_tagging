e4136ea647200f814cb44f3f5e28cae7387a08ef
yong xu @ surrey <xuyong62@mail.ustc.edu.cn>
2016-11-16 18:59:19 +0000

    Update config_fb40.py

--- config_fb40.py
+++ config_fb40.py
@@ -1,20 +1,16 @@
 
 
 # development
-ori_dev_root = '/vol/vssp/datasets/audio/dcase2016/chime_home'
-#dev_root = '/vol/vssp/datasets/audio/dcase2016/chime_home'
-#dev_wav_fd = dev_root + '/chunks'
+ori_dev_root = 'your_dir/chime_home'
 
-dev_root = '/vol/vssp/msos/yx/chime_home/DCASE2016_task4_scrap_1ch_mfcc/Fe'
+dev_root = 'your_dir/Fe'
 dev_wav_fd = dev_root + '/DCASE2016_task4_16k_W20msS10ms_fbank40'
 
 # temporary data folder
-scrap_fd = "/vol/vssp/msos/yx/chime_home/DCASE2016_task4_scrap_1ch_mfcc"
+scrap_fd = "your_dir/DCASE2016_task4_scrap_1ch_mfcc"
 dev_fe_mel_fd = scrap_fd + '/Fe/htk_fb40'
-#dev_fe_mel_fd = scrap_fd + '/Fe/dae_relu40'
 dev_cv_csv_path = ori_dev_root + '/development_chunks_refined_crossval_dcase2016.csv'
-#dev_cv_csv_path = ori_dev_root + '/development_chunks_raw_crossval_dcase2016.csv'
-#dev_cv_csv_path = ori_dev_root + '/development_chunks_raw_crossval_dcase2016_eval816.csv' # for evalset
+
 
 
 # evaluation
